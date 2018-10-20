382b7bc375653edb0a2e24645b3f26e014912b7d
Brad Robinson <contact@toptensoftware.com>
2018-10-20 12:33:37 +1100

    NuGet Package

---
+++ build.js
@@ -0,0 +1,42 @@
+var bt = require('./BuildTools/buildTools.js')
+
+// Crack /debug /release and /all options
+var debug = bt.options.switches.debug || bt.options.switches.all;
+var release = bt.options.switches.release || bt.options.switches.all;
+if (!debug && !release)
+	release = true;
+
+// Clock version
+bt.version();
+bt.clock_version();
+
+// Don't bother cleaning if we're not doing a release build
+if (release)
+{
+	bt.options.clean = true;
+	bt.clean(".\\Build");
+}
+
+// Debug build
+if (debug)
+{
+	bt.dnbuild("Debug");
+	bt.nupack("PetaJson.Debug.nuspec", ".\\Build");
+}
+
+// Release build
+if (release)
+{
+	bt.dnbuild("Release");
+
+	bt.signfile([
+        "Build\\Release\\PetaJson\\netcoreapp2.0\\PetaJson.dll",
+        "Build\\Release\\PetaJson\\net461\\PetaJson.dll",
+
+    ], "PetaJson JSON Serialization Library");
+
+	bt.nupack("PetaJson.nuspec", ".\\Build");
+}
+
+
+bt.copy("build\\*.nupkg", "\\\\cool\\public\\ToptenNuget")
