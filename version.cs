d41c565b0d660f0c806afdae38092b73e02f0eeb
Brad Robinson <contact@toptensoftware.com>
2020-06-23 11:44:30 +1000

    b1.1.120

--- version.cs
+++ version.cs
@@ -3,13 +3,13 @@
 using System;
 using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2014-2020 Topten Software. All Rights Reserved")]
-[assembly: AssemblyVersion("1.1.119")]
-[assembly: AssemblyFileVersion("1.1.119")]
+[assembly: AssemblyVersion("1.1.120")]
+[assembly: AssemblyFileVersion("1.1.120")]
 [assembly: AssemblyCompany("Topten Software")]
 [assembly: AssemblyProduct("JsonKit")]
 
 static class BuildInfo
 {
-	public static DateTime Date = new DateTime(2020, 6, 23, 1, 39, 34, DateTimeKind.Utc);
+	public static DateTime Date = new DateTime(2020, 6, 23, 1, 44, 7, DateTimeKind.Utc);
 }
 	
