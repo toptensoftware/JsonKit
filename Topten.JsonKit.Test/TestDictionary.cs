1528361be90edd00b4b8b2b3fc4124a2f5edf8a4
Brad Robinson <contact@toptensoftware.com>
2021-09-14 12:02:54 +1000

    Unit test for int key dictionaries

---
+++ Topten.JsonKit.Test/TestDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topten.JsonKit;
+using Xunit;
+
+namespace TestCases
+{
+    public class TestDictionary
+    {
+        [Fact]
+        public void DictionaryNonStringKeys()
+        {
+            var dict = new Dictionary<int, double>()
+            {
+                [-1] = 50,
+                [0] = 100,
+                [1] = 200,
+            };
+
+            var json = Json.Format(dict);
+
+            var dict2 = Json.Parse<Dictionary<int, double>>(json);
+
+            Assert.Equal(dict, dict2);
+        }
+    }
+}
